c5d691120a8b1f8fc88ed67d2c275f364e7d77c7
MRPGMR <oroszp26@gmail.com>
2024-05-19 17:19:26 +0200

    mappaszerk

---
+++ parfumeapp/src/main/java/com/parfumeapp/parfumeapp/ParfumeappApplication.java
@@ -0,0 +1,13 @@
+package com.parfumeapp.parfumeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParfumeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParfumeappApplication.class, args);
+	}
+
+}
---
+++ parfumeapp/src/test/java/com/parfumeapp/parfumeapp/ParfumeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parfumeapp.parfumeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParfumeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
