af5555be7f6a1b87d71bee3abd37becea54c152e
MRPGMR <oroszp26@gmail.com>
2024-05-20 19:23:37 +0200

    Update PerfumeController.java

--- src/main/java/com/parfumeapp/parfumeapp/controller/PerfumeController.java
+++ src/main/java/com/parfumeapp/parfumeapp/controller/PerfumeController.java
@@ -3,7 +3,15 @@ package com.parfumeapp.parfumeapp.controller;
 import com.parfumeapp.parfumeapp.model.Perfume;
 import com.parfumeapp.parfumeapp.service.PerfumeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
