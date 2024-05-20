b1eba31f8808df5d5e46d7a2dc574ab0eb95f8db
MRPGMR <oroszp26@gmail.com>
2024-05-20 19:25:33 +0200

    Update PerfumeRepository.java

--- src/main/java/com/parfumeapp/parfumeapp/repository/PerfumeRepository.java
+++ src/main/java/com/parfumeapp/parfumeapp/repository/PerfumeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PerfumeRepository extends JpaRepository <Perfume,Integer> {
+public interface PerfumeRepository extends JpaRepository <Perfume, Integer> {
 }
